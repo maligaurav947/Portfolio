da7c906575d423f79aa879965648b645c1d7f3c8
Mali Gaurav <maligaurav947@gmail.com>
2022-09-20 10:29:04 +0530

    V0.5

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import Header from "./js-asst/Header";
 import Hero from "./js-asst/Hero";
 import Project from "./js-asst/Project";
 import SkillList from "./js-asst/SkillList";
+import { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <Hero />
-      <Project />
-      <SkillList />
-      <About />
-      <Contact />
-      <Header />
+      {loading ? (
+        <PacmanLoader
+          color={"#e2ff02"}
+          loading={loading}
+          size={100}
+          className="loader"
+        />
+      ) : (
+        <div>
+          <Hero />
+          <Project />
+          <SkillList />
+          <About />
+          <Contact />
+          <Header />
+        </div>
+      )}
     </div>
   );
 }
