a61dd71acd1d0e214894c241d3f2285ffbfa4f64
Mali Gaurav <maligaurav947@gmail.com>
2022-09-29 01:02:21 +0530

    V0.6

---
+++ src/Working.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Working() {
+  return (
+    <div className="main-working">
+      <img src="https://www.fg-a.com/under-construction/construction-worker-animation.gif" />
+      <h1>Sorry For Your Inconvenience</h1>
+      <p>This Site Is Under Construction</p>
+    </div>
+  );
+}
