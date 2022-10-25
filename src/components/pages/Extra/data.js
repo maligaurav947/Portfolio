f1dde747a6581dbd8f81a6af6ae425db85d15c0a
Mali Gaurav <maligaurav947@gmail.com>
2022-10-25 23:20:20 +0530

    New UI

---
+++ src/components/pages/Extra/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    img: "https://user-images.githubusercontent.com/86152880/197372331-04bcc17f-1e95-4e80-b078-460ba3bf4d46.png",
+    title: "Libary List ",
+    info: "",
+    link: "https://reactjs.org/",
+  },
+  {
+    img: "",
+    title: "Toy Shop",
+    info: "",
+    link: "https://reactjs.org/",
+  },
+  {
+    img: "",
+    title: "Bank For Developer",
+    info: "",
+    link: "https://reactjs.org/",
+  },
+  {
+    img: "",
+    title: "Show Case",
+    info: "",
+    link: "https://reactjs.org/",
+  },
+];
+
+export default data;
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,33 @@
 import React from "react";
 import Header from "./Header";
-
+import data from "./Extra/data";
 function Project() {
   return (
     <>
       <Header />
-      <section>Project</section>
+      <section className="project-section">
+        <h3 className="section-title">
+          Have A <span className="design-text">Look</span> What I am Working On
+        </h3>
+        <main className="project-section__main">
+          {data.map((data) => (
+            <div className="project-section__main__card">
+              <img src={data.img} />
+              <div className="project-section__main__card__text">
+                <h4 className="project-section__main__card__text__title">
+                  {data.title}
+                </h4>
+                <p className="section-info">{data.info}</p>
+                <a href={data.link}>
+                  <p className="project-section__main__card__text__btn">
+                    &lt;Vist Website /&gt;
+                  </p>
+                </a>
+              </div>
+            </div>
+          ))}
+        </main>
+      </section>
     </>
   );
 }
