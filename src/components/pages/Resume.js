055b98bbb65f73b667a0b3e0533ceba56f7491c1
Mali Gaurav <maligaurav947@gmail.com>
2022-10-31 10:10:58 +0530

    V0.4

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,13 +6,19 @@ function Resume() {
     <>
       <Header />
       <section className="resume-section">
-        <a href="https://drive.google.com/file/d/1e9m6clQLvXkxqS5r3nFDfDFy8JA4TY27/view">
+        <a
+          href="https://drive.google.com/file/d/1e9m6clQLvXkxqS5r3nFDfDFy8JA4TY27/view"
+          target={"_blank"}
+        >
           <button className="hero-section__text__btn">
             &lt;Download /&gt;
           </button>
         </a>
         <main></main>
-        <a href="https://drive.google.com/file/d/1e9m6clQLvXkxqS5r3nFDfDFy8JA4TY27/view">
+        <a
+          href="https://drive.google.com/file/d/1e9m6clQLvXkxqS5r3nFDfDFy8JA4TY27/view"
+          target={"_blank"}
+        >
           <button className="hero-section__text__btn">
             &lt;Download /&gt;
           </button>
