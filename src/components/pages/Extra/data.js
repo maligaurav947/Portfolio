db9e80c7c0fce50fb9ec615ddcb1909135aee0b9
Mali Gaurav <maligaurav947@gmail.com>
2022-11-11 09:43:31 +0530

    V0.6 Project

--- src/components/pages/Extra/data.js
+++ src/components/pages/Extra/data.js
@@ -1,5 +1,17 @@
 const data = [
   {
+    img: "https://github.com/maligaurav947/Portfolio/blob/master/src/res/screencapture-maligaurav947-github-io-news-2022-11-11-09_32_37.png?raw=true",
+    title: "News",
+    info: "Collection of New / Breaking News Collected By News Data API",
+    link: "https://maligaurav947.github.io/news/",
+  },
+  {
+    img: "https://github.com/maligaurav947/Portfolio/blob/master/src/res/screencapture-maligaurav947-github-io-web-kit-2022-11-11-09_33_01.png?raw=true",
+    title: "Web Developer Kit",
+    info: "The Project Content All Type Of Tools / Kit's And Show Them One Place Just Collection Of Tools",
+    link: "https://maligaurav947.github.io/web-kit/",
+  },
+  {
     img: "https://user-images.githubusercontent.com/86152880/197372331-04bcc17f-1e95-4e80-b078-460ba3bf4d46.png",
     title: "Libary List ",
     info: "The Project Content All Type Of Library.The Basic Goal of This Project The Collect All Type Of Library And Show Them One Place",
